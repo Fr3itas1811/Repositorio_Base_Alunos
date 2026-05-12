30ed1923d05cc8c22628e036c824bd9805c4b4fc
Fr3itas1811 <duhenriquefreitas1811@gmail.com>
2026-05-12 08:39:21 -0300

    Add files via upload

---
+++ Modulo 1/Loopings/primeiro.py
@@ -0,0 +1,11 @@
+# variaveis
+#inteiro, real, cadeia, logico
+rpm = 2000 #variavel rpm guarda 2000 (inteiro)
+preco_pasagem = 7.50 #variavel preco_pasagem guarda 6.7 (real)
+produto = "MOCHILA ADIDAS" #variavel produto guarda "mochila adidas" (cadeia)
+ehmaioridade = True #variavel ehmaioridade guarda true (logico)
+
+
+
+
+
---
+++ Modulo 1/Loopings/segundo.py
@@ -0,0 +1,50 @@
+#operadores aritimeticos
+# + - soma
+# - - sutraçao
+# / - divisao
+# * - multiplicaçao
+# % - resto da divisao
+# ** - elevado
+
+# print(5 + 5)
+# print(4 - 2)
+# print(10 / 2)
+# print(4 * 2)
+# print(50 % 2)
+# print(5 ** 2)
+
+# numero1 = float( input("digite o primeiro numero: ") )
+# numero2 = float( input("digite o segundo numero:") )
+
+# print(numero1 + numero2)
+# print(numero1 - numero2)
+# print(numero1 / numero2)
+# print(numero1 * numero2)
+# print(numero1 % numero2)
+# print(numero1 ** numero2)
+
+
+
+
+# nome = input("Digite o nome do aluno: ")
+
+# nota1 = float(input("Digite a primeira nota: "))
+# nota2 = float(input("Digite a segunda nota: "))
+# nota3 = float(input("Digite a terceira nota: "))
+# nota4 = float(input("Digite a quarta nota: "))
+
+# media = (nota1 + nota2 + nota3 + nota4) / 4
+
+
+# if media >= 6:
+#  print('então ele esta aprovado')
+# else: media <= 6 ; 
+# print('então ele esta reprovado')
+# print(o aluno foi reprovado)
+
+
+
+
+
+,
+  
